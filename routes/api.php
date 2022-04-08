9f0db1e4f1192b48bd396cb560cb7de879845070
Matias Piuma <matius@gmail.com>
2022-04-08 14:52:37 +0200

    Find employee by id

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use Api\V1\EmployeeContext\Infrastructure\Controllers\CreateEmployeeController;
+use Api\V1\EmployeeContext\Infrastructure\Controllers\FindEmployeeController;
 use Api\V1\EmployeeContext\Infrastructure\Controllers\GetAllEmployeesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get(uri: '/employees', action: GetAllEmployeesController::class);
 
 Route::post(uri: '/employees', action: CreateEmployeeController::class);
+
+Route::get(uri: '/employees/{employeeId}', action: FindEmployeeController::class);
---
+++ src/V1/EmployeeContext/Infrastructure/Controllers/FindEmployeeController.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Api\V1\EmployeeContext\Infrastructure\Controllers;
+
+use Api\V1\EmployeeContext\Application\FindEmployee\FindEmployeeQuery;
+use Api\V1\SharedContext\Application\CQRS\Query\QueryBusInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+final class FindEmployeeController
+{
+    public function __construct(
+        private QueryBusInterface $queryBus
+    ) {
+    }
+    public function __invoke(
+        string $employeeId
+    ): JsonResponse {
+        $employee = $this->queryBus->execute(
+            new FindEmployeeQuery(
+                id: $employeeId
+            )
+        );
+
+        return new JsonResponse(
+            data: [
+                'data' => $employee->toPrimitives(),
+                'meta' => []
+            ],
+            status: Response::HTTP_OK
+        );
+    }
+}
