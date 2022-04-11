9326ca028c656dfa29f0c578c26925ae65287c72
Matias Piuma <matius@gmail.com>
2022-04-11 17:53:49 +0200

    [refactor] A little decouple from the framework

--- app/Http/Controllers/Api/V1/Employees/PostEmployeeController.php
+++ app/Http/Controllers/Api/V1/Employees/PostEmployeeController.php
@@ -6,28 +6,15 @@ namespace App\Http\Controllers\Api\V1\Employees;
 
 use Api\V1\EmployeeContext\Employee\Infrastructure\Controllers\CreateEmployeeController;
 use App\Http\Requests\Api\V1\Employees\CreateEmployeeRequest;
-use App\Models\Employee;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 
 final class PostEmployeeController
 {
-    public function __construct(
-        private CreateEmployeeController $controller
-    )
-    {
-    }
-
     public function __invoke(
-        CreateEmployeeRequest $request
+        CreateEmployeeRequest $request,
+        CreateEmployeeController $controller
     ): JsonResponse
     {
-        /** @var Employee $response */
-        $response = $this->controller->__invoke($request);
-
-        return new JsonResponse(
-            ['data' => $response->toPrimitives()],
-            status: Response::HTTP_CREATED
-        );
+        return $controller($request);
     }
 }
--- src/V1/EmployeeContext/Employee/Infrastructure/Controllers/CreateEmployeeController.php
+++ src/V1/EmployeeContext/Employee/Infrastructure/Controllers/CreateEmployeeController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Api\V1\EmployeeContext\Employee\Infrastructure\Controllers;
 
 use Api\V1\EmployeeContext\Employee\Application\CreateEmployee\CreateEmployeeCommand;
+use Api\V1\EmployeeContext\Employee\Application\FindEmployeeById\FindEmployeeByIdQuery;
 use Api\V1\EmployeeContext\Employee\Domain\Employee;
 use Api\V1\EmployeeContext\Employee\Infrastructure\Responses\EmployeeResponseInterface;
 use Api\V1\EmployeeContext\Shared\Domain\Employee\EmployeeId;
@@ -12,6 +13,7 @@ use Api\V1\SharedContext\Application\CQRS\Command\CommandBusInterface;
 use Api\V1\SharedContext\Application\CQRS\Query\QueryBusInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 final class CreateEmployeeController
 {
@@ -24,16 +26,26 @@ final class CreateEmployeeController
 
     public function __invoke(
         Request $request
-    ): Employee
+    ): JsonResponse
     {
         $employeeId = EmployeeId::make();
 
-        return $this->commandBus->execute(
+        $this->commandBus->execute(
             new CreateEmployeeCommand(
                 $employeeId->id,
                 $request->get('name'),
                 $request->get('email'),
             )
+        );
+
+        /** @var Employee $response */
+        $response = $this->queryBus->execute(
+            new FindEmployeeByIdQuery($employeeId->value())
+        );
+
+        return new JsonResponse(
+            ['data' => $response->toPrimitives()],
+            status: Response::HTTP_CREATED
         );
     }
 }
